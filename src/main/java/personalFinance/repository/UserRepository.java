8ca098731c0713498ec4a55258cf070cdbb5f480
alnero <darestream@gmail.com>
2019-05-27 10:24:58 +0300

    Add spring data dependency, add application.properties and UserRepository interface

---
+++ src/main/java/personalFinance/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package personalFinance.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import personalFinance.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserById(Long id);
+}
