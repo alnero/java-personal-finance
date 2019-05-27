66a1f4420c886469467927a4ca72d0cc26245736
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2019-05-27 21:07:47 +0300

    Clean up code

--- src/main/java/personalFinance/controller/UserController.java
+++ src/main/java/personalFinance/controller/UserController.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import personalFinance.model.User;
 import personalFinance.repository.UserRepository;
-import java.util.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -52,46 +53,4 @@ public class UserController {
         user = this.userRepository.save(user);
         return user;
     }
-
-
-    /*
-    private Map<Long, User> userMap = new HashMap<>();
-
-    @ResponseBody
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<User> getUsers() {
-        Collection<User> collection = userMap.values();
-        List<User> list = new ArrayList<>();
-        list.addAll(collection);
-        return list;
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "{userId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-    public User updateUser(@PathVariable("userId") Long userId) throws UserNotFoundException {
-        User user = this.userMap.get(userId);
-        if (user == null) {
-            throw new UserNotFoundException("User with id=" + userId + " not found");
-        }
-        user.setName("Some random content which updates user.");
-        this.userMap.put(userId, user);
-        return user;
-    }
-
-    @ResponseBody
-    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public User createUser(@RequestBody String userContent) {
-        Long userId = ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE);
-        User user = new User(userId, userContent);
-        this.userMap.put(userId, user);
-        return user;
-    }
-
-
-    @ResponseBody
-    @RequestMapping(value = "{userId}", method = RequestMethod.DELETE)
-    public void deleteUser(@PathVariable("userId") Long userId) {
-        this.userMap.remove(userId);
-    }
-    */
 }
--- src/main/java/personalFinance/repository/UserRepository.java
+++ src/main/java/personalFinance/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package personalFinance.repository;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import personalFinance.model.User;
 
 @Repository
